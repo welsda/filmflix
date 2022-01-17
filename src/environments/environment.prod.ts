e896eb0d622fd10d8d5e130ee4803cb6a382a482
Wellington Silva de Almeida <wellingtonsilvadealmeida1995@gmail.com>
2022-01-17 18:08:47 -0300

    Adição de configurações do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-fc0d6',
+    appId: '1:976240981602:web:e3ec16c597dd0c6ec0033b',
+    storageBucket: 'filmflix-fc0d6.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDyXqowg744QzGGiwhoBK3BscYgVISoTLg',
+    authDomain: 'filmflix-fc0d6.firebaseapp.com',
+    messagingSenderId: '976240981602',
+  },
   production: true
 };
